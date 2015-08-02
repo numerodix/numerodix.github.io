0513e7eb8c5061304a07edf5a937905f21f064b8
Martin Matusiak <numerodix@gmail.com>
2015-08-03 01:19:38 +0200

    use variables for mnemonics

--- js/data_definitions.js
+++ js/data_definitions.js
@@ -4,6 +4,15 @@ var vecAlternating = 0x55;
 var vecSelected = 0x30;
 var vecFull = 0xff;
 
+var mnemNoChange = "no change";
+var mnenProducesEmpty = "produces empty";
+var mnenInvert = "inverts";
+var mnenTestSelected = "tests the selected bits";
+var mnenTurnOnAll = "turns on all bits";
+var mnenTurnOnSelected = "turns on the selected bits";
+var mnenTurnOffAll = "turns off all bits";
+var mnenToggleSelected = "toggles selected bits";
+
 
 // AND
 
@@ -12,42 +21,42 @@ bithackCollection.createOperation(
         "and",
         vecAlternating,
         vecEmpty,
-        "turns off all bits");
+        mnenTurnOffAll);
 
 bithackCollection.createOperation(
         "alternating AND full",
         "and",
         vecAlternating,
         vecFull,
-        "no change");
+        mnemNoChange);
 
 bithackCollection.createOperation(
         "alternating AND a given vector",
         "and",
         vecAlternating,
         vecSelected,
-        "tests the selected bits");
+        mnenTestSelected);
 
 bithackCollection.createOperation(
         "full AND empty",
         "and",
         vecFull,
         vecEmpty,
-        "inverts");
+        mnenInvert);
 
 bithackCollection.createOperation(
         "full AND full",
         "and",
         vecFull,
         vecFull,
-        "no change");
+        mnemNoChange);
 
 bithackCollection.createOperation(
         "alternating AND self",
         "and",
         vecAlternating,
         vecAlternating,
-        "no change");
+        mnemNoChange);
 
 
 // OR
@@ -57,42 +66,42 @@ bithackCollection.createOperation(
         "or",
         vecAlternating,
         vecEmpty,
-        "no change");
+        mnemNoChange);
 
 bithackCollection.createOperation(
         "alternating OR full",
         "or",
         vecAlternating,
         vecFull,
-        "turns on all bits");
+        mnenTurnOnAll);
 
 bithackCollection.createOperation(
         "alternating OR a given vector",
         "or",
         vecAlternating,
         vecSelected,
-        "turns on selected bits");
+        mnenTurnOnSelected);
 
 bithackCollection.createOperation(
         "full OR empty",
         "or",
         vecFull,
         vecEmpty,
-        "no change");
+        mnemNoChange);
 
 bithackCollection.createOperation(
         "full OR full",
         "or",
         vecFull,
         vecFull,
-        "no change");
+        mnemNoChange);
 
 bithackCollection.createOperation(
         "alternating OR self",
         "or",
         vecAlternating,
         vecAlternating,
-        "no change");
+        mnemNoChange);
 
 
 // XOR
@@ -102,39 +111,39 @@ bithackCollection.createOperation(
         "xor",
         vecAlternating,
         vecEmpty,
-        "no change");
+        mnemNoChange);
 
 bithackCollection.createOperation(
         "alternating XOR full",
         "xor",
         vecAlternating,
         vecFull,
-        "inverts");
+        mnenInvert);
 
 bithackCollection.createOperation(
         "alternating XOR a given vector",
         "xor",
         vecAlternating,
         vecSelected,
-        "toggles selected bits");
+        mnenToggleSelected);
 
 bithackCollection.createOperation(
         "full XOR empty",
         "xor",
         vecFull,
         vecEmpty,
-        "no change");
+        mnemNoChange);
 
 bithackCollection.createOperation(
         "full XOR full",
         "xor",
         vecFull,
         vecFull,
-        "flips all / inverts");
+        mnenInvert);
 
 bithackCollection.createOperation(
         "alternating XOR self",
         "xor",
         vecAlternating,
         vecAlternating,
-        "produces empty");
+        mnenProducesEmpty);
